8e985d185bbcdb603d12e9dd4b49573301940d6b
Reza Lael <rezalaal@gmail.com>
2025-07-22 11:36:10 +0330

    refactor docker test database

--- internal/integration/docker_test_database_connection_test.go
+++ internal/integration/docker_test_database_connection_test.go
@@ -2,25 +2,22 @@ package integration_test
 
 import (
 	"database/sql"
-	"os"
-	
-	"path/filepath"
-
 	"testing"
-
-	"github.com/joho/godotenv"
+	
 	_ "github.com/lib/pq" // PostgreSQL driver
+	"github.com/rezalaal/coral/config"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestDatabaseConnection(t *testing.T) {
-	// تنظیمات اتصال به پایگاه داده
-	rootPath, _ := filepath.Abs(filepath.Join("..", "..", ".env"))
-	err := godotenv.Load(rootPath)
+	// بارگذاری تنظیمات از فایل config
+	cfg, err := config.Load()
 	assert.NoError(t, err)
-	connStr := os.Getenv("DATABASE_URL")
+
+	// استفاده از DATABASE_URL از config
+	connStr := cfg.DatabaseURL
 	db, err := sql.Open("postgres", connStr)
 	require.NoError(t, err)
 	defer db.Close()
