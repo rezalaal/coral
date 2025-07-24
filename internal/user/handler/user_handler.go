217f0f7c74a997686a9f46297e18d452b87756ba
Reza Lael <rezalaal@gmail.com>
2025-07-24 20:55:02 +0330

    refactor user_handler & test utils

--- internal/user/handler/user_handler.go
+++ internal/user/handler/user_handler.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/rezalaal/coral/internal/user/models"
 	"github.com/rezalaal/coral/internal/user/repository/interfaces"
+	"github.com/rezalaal/coral/internal/utils"
 )
 
 type UserHandler struct {
@@ -43,23 +44,6 @@ func isValidMobile(mobile string) bool {
 	return regexp.MustCompile(`^\d{11}$`).MatchString(mobile)
 }
 
-// تابع برای تبدیل اعداد فارسی به انگلیسی
-func convertPersianToEnglishNumbers(input string) string {
-	replacement := map[rune]rune{
-		'۰': '0', '۱': '1', '۲': '2', '۳': '3', '۴': '4',
-		'۵': '5', '۶': '6', '۷': '7', '۸': '8', '۹': '9',
-	}
-
-	var result []rune
-	for _, char := range input {
-		if englishChar, exists := replacement[char]; exists {
-			result = append(result, englishChar)
-		} else {
-			result = append(result, char)
-		}
-	}
-	return string(result)
-}
 
 func (h *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
 	var user models.User
@@ -76,7 +60,7 @@ func (h *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// تبدیل شماره موبایل به انگلیسی در صورت لزوم
-	user.Mobile = convertPersianToEnglishNumbers(user.Mobile)
+	user.Mobile = utils.ConvertPersianToEnglishNumbers(user.Mobile)
 
 	// اعتبارسنجی ورودی‌ها
 	if !isValidName(user.Name) {
---
+++ internal/utils/utils_test.go
@@ -0,0 +1,27 @@
+// internal/utils/utils_test.go
+package utils
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+	"github.com/rezalaal/coral/internal/utils"
+)
+
+func TestConvertPersianToEnglishNumbers(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{"۰۱۲۳۴۵۶۷۸۹", "0123456789"},
+		{"Hello 1234", "Hello 1234"},
+		{"سلام ۱۲۳۴", "سلام 1234"},
+	}
+
+	for _, test := range tests {
+		t.Run(test.input, func(t *testing.T) {
+			// فراخوانی تابع از پکیج utils
+			result := utils.ConvertPersianToEnglishNumbers(test.input)
+			assert.Equal(t, test.expected, result)
+		})
+	}
+}
